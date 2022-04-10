e1c6eb9279459ee78de94ee0be4430fe57907d73
Paulo Fernando <paulo.fdo@hotmail.com>
2022-04-10 17:08:50 -0300

    DataGridComponent + Atualização da Página

--- BlazoriseDGNorthwindDemo/NorthwindDemo.Client/Pages/Orders.razor.cs
+++ BlazoriseDGNorthwindDemo/NorthwindDemo.Client/Pages/Orders.razor.cs
@@ -1,58 +1,11 @@
-﻿using Blazorise;
-using Blazorise.DataGrid;
-using Microsoft.AspNetCore.Components;
+﻿using NorthwindDemo.Client.Components;
 using NorthwindDemo.Client.Services;
-using NorthwindDemo.Infrastructure.Shared.Pagination;
 using NorthwindDemo.Models;
 
 namespace NorthwindDemo.Client.Pages
 {
-    public partial class Orders
+    public class OrdersComponent : DataGridComponent<OrderDto, IOrderDataService>
     {
-        [Inject]
-        private IOrderDataService OrderDataService { get; set; }
-        protected List<OrderDto> OrderList { get; set; } = new List<OrderDto>();
-        protected int Total { get; set; }
-        protected OrderDto SelectedOrder { get; set; }
-        private async Task OnReadData(DataGridReadDataEventArgs<OrderDto> e)
-        {
-            if (!e.CancellationToken.IsCancellationRequested)
-            {
-                var pagedQueryModel = GetPagedQueryModel(e);
-
-                if (!e.CancellationToken.IsCancellationRequested)
-                {
-                    var pagedResult = await OrderDataService.GetData(pagedQueryModel);
-
-                    Total = pagedResult.RowCount;
-                    OrderList = pagedResult.Items.ToList();
-                }
-            }
-        }
-        private static PagedQueryModel GetPagedQueryModel(DataGridReadDataEventArgs<OrderDto> e)
-        {
-            var result = e.ReadDataMode == DataGridReadDataMode.Virtualize ? new PagedQueryModel()
-            {
-                Page = e.VirtualizeOffset,
-                PageSize = e.VirtualizeCount
-            } : e.ReadDataMode == DataGridReadDataMode.Paging ? new PagedQueryModel()
-            {
-                Page = (e.Page - 1) * e.PageSize,
-                PageSize = e.PageSize
-            } : default;
-
-            if (result == null)
-                throw new Exception("Unhandled ReadDataMode");
-
-            result.SortColumns = e.Columns.Where(x => x.SortDirection != SortDirection.Default)
-                                     .OrderBy(x => x.SortIndex)
-                                     .Select(x => new PagedQueryColumnModel
-                                     {
-                                         Name = x.Field,
-                                         Direction = x.SortDirection == SortDirection.Ascending ? "asc" : "desc"
-                                     }).ToArray();
-
-            return result;
-        }
+        protected override string Title => "Orders";
     }
 }
